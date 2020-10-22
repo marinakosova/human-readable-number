bfb6b5067e6307a5bb7f847b060964145e082be8
marinakosova <marina.kosova@gmail.com>
2020-10-22 11:23:39 -0500

    feat: add toreadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,107 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number == 0)
+        return "zero";
+    return three(number);
+
+    function two(num) {
+        if (num == 0)
+            return "";
+        else if (num < 10)
+            return one(num);
+        else if (num < 20)
+            return twoLessThan20(num);
+        else {
+            let tenner = Math.floor(num / 10);
+            let rest = num - tenner * 10;
+            if (rest != 0)
+                return ten(tenner) + " " + one(rest);
+            else
+                return ten(tenner);
+        }
+    }
+
+    function three(num) {
+        let hundred = Math.floor(num / 100);
+        rest = num - hundred * 100;
+        let res = "";
+        if (hundred != 0 && rest != 0)
+            res = one(hundred) + " hundred " + two(rest);
+        else if ((hundred == 0) && (rest != 0))
+            res = two(rest);
+        else if ((hundred != 0) && (rest == 0))
+            res = one(hundred) + " hundred";
+        return res;
+    }
+
+    function one(num) {
+        switch (num) {
+            case 1:
+                return "one";
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            case 9:
+                return "nine";
+        }
+        return "";
+    }
+
+    function twoLessThan20(num) {
+        switch (num) {
+            case 10:
+                return "ten";
+            case 11:
+                return "eleven";
+            case 12:
+                return "twelve";
+            case 13:
+                return "thirteen";
+            case 14:
+                return "fourteen";
+            case 15:
+                return "fifteen";
+            case 16:
+                return "sixteen";
+            case 17:
+                return "seventeen";
+            case 18:
+                return "eighteen";
+            case 19:
+                return "nineteen";
+        }
+        return "";
+    }
+
+    function ten(num) {
+        switch (num) {
+            case 2:
+                return "twenty";
+            case 3:
+                return "thirty";
+            case 4:
+                return "forty";
+            case 5:
+                return "fifty";
+            case 6:
+                return "sixty";
+            case 7:
+                return "seventy";
+            case 8:
+                return "eighty";
+            case 9:
+                return "ninety";
+        }
+        return "";
+    }
 }
